c328b972939f9460437cccdd29ac411753af9817
nthieme <nick.thieme5@gmail.com>
2018-11-27 10:49:06 -0500

    changes to queries and code to include time chunks in the Mapbox code.

--- mapbox_pipeline_wrapper.R
+++ mapbox_pipeline_wrapper.R
@@ -15,26 +15,35 @@ setwd("Set this to the directory")
 ################################
 
 load("MLab_data_census_tract")
-load("477_data")
+load("D_477_2017_june")
+load("D_477_2015_dec")
 load("MLab_data_state_house")
 load("MLab_data_state_senate")
+load("totalpop_sf_county")
+load("totalpop_sf_tract")
+load("legislative_mlab")
 
 #change max to copies below to get averages or todo_avg_max to get the averaged maxes
+D_477_2015_dec_p<-process_477(D_477_2015_dec)
+names(D_477_2017_june)[1]<-"GEOID"
+D_477<-bind_rows(data.frame(D_477_2015_dec_p,date_range="dec_15"),
+                 data.frame(D_477_2017_june, date_range="dec_16"))
+D_477<-D_477[,c(1,2,3,4,5)]
 names(D_477)[1]<-"GEOID"
 D_477$GEOID<-as.character(D_477$GEOID)
 D_477$GEOID<-as.factor(D_477$GEOID)
+
 D_joined<-left_join(todo_copies, totalpop_sf, by = "tract")
 D_mlab_census_shape<-left_join(D_477, totalpop_sf)%>%na.omit
-D_mlab_census_shape_477<-left_join(D_mlab_census_shape, D_joined, by = "GEOID")
+D_mlab_census_shape_477<-left_join(D_mlab_census_shape, D_joined, by = c("GEOID", "date_range"))
 
-D_plot_f<-D_mlab_census_shape_477[,c(1,3,9,10,11,12,13,14)]
-geoms <- D_plot_f[,c(1,3)]%>%na.omit%>%distinct
+D_plot_f<-D_mlab_census_shape_477[,c(1,3,5,6,10,11,13,14,15)]
+geoms <- D_plot_f[,c(1,5)]%>%na.omit%>%distinct
 
-geojson_mlab<-D_plot_f%>%filter(between(day, ymd("2016-06-01"), ymd("2017-07-15")))%>%
-  group_by(GEOID)%>%
-  summarise(mlab_speed=median(med_speed, na.rm = TRUE), 
+geojson_mlab<-D_plot_f%>%group_by(GEOID)%>%summarise(mlab_speed=median(med_speed, na.rm = TRUE), 
             counts = sum(tract_test_counts, na.rm = TRUE))
-geojson_m<-left_join(data.frame(GEOID=D_plot_f$GEOID, med_dl=D_plot_f$med_dl), geojson_mlab)%>%
+geojson_m<-left_join(data.frame(GEOID=D_plot_f$GEOID, med_dl=D_plot_f$med_dl,
+                                date_range=D_plot_f$date), geojson_mlab)%>%
   distinct
 geojson_final <-left_join(geojson_m, geoms, by = "GEOID")
 
@@ -49,7 +58,7 @@ m_lab_477_final_county <- mlab_speeds%>%mutate(county = str_sub(GEOID, 1,5))
 uniq_county <- unique(m_lab_477_final_county$county)
 county <- m_lab_477_final_county$county
 
-m_lab_477_final_county_grouped<-m_lab_477_final_county%>%group_by(county)%>%
+m_lab_477_final_county_grouped<-m_lab_477_final_county%>%group_by(county, date_range)%>%
   summarise(
     med_dl=median(med_dl,na.rm=TRUE), 
     mlab_speed=median(mlab_speed,na.rm=TRUE),
@@ -66,7 +75,7 @@ D_new<-data.frame(geom = simplepolys$geometry, county = simplepolys$county)%>%st
 st_write(D_new, "mapbox_geometry_county.geojson")
 
 #use for zcta
-names(D)<-c("county","a","a","a","a","speed_477","speed_mlab","counts","speed_diff","geom")
+names(D)<-c("county","a","a","a","a","date_range","speed_477","speed_mlab","counts","speed_diff","geom")
 st_geometry(D) <- "geom"
 
 D_data<-data.frame(county = D$county, speed_mlab=D$speed_mlab, speed_477=D$speed_477, 
@@ -82,10 +91,10 @@ m_lab_477_final_sf <- st_as_sf(mlab_speeds)
 st_crs(m_lab_477_final_sf)<-st_crs(df_final)
 m_lab_477_final_leg_sf<-st_join(m_lab_477_final_sf, df_final)
 
-m_lab_final_leg_dis<-m_lab_477_final_leg_sf%>%group_by(GEOID.y, FUNCSTAT)%>%
+m_lab_final_leg_dis<-m_lab_477_final_leg_sf%>%group_by(GEOID.y, FUNCSTAT, date_range)%>%
   summarise(
-    med_dl.x=mean(med_dl.x),
-    med_dl.y=sum(na.omit(med_dl.y*counts))/(sum(na.omit(counts))),
+    med_dl.x=mean(med_dl),
+    med_dl.y=sum(na.omit(mlab_speed*counts))/(sum(na.omit(counts))),
     counts=sum(na.omit(counts)),
     house = FUNCSTAT[1]
   )%>%mutate(
@@ -93,4 +102,5 @@ m_lab_final_leg_dis<-m_lab_477_final_leg_sf%>%group_by(GEOID.y, FUNCSTAT)%>%
   )
 
 D_state_house_med<-D_state_house%>%group_by(GEOID)%>%summarise(med = median(med_speed))
+D_state_senate_med<-D_state_senate%>%group_by(GEOID)%>%summarise(med = median(med_speed))
 D_states<-left_join(dataflow_lower_df, D_state_house_med)
--- pipeline_functions.R
+++ pipeline_functions.R
@@ -2,6 +2,8 @@
 #|Functions for United States of Broadband Pipeline|#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
+project <- "mlab-sandbox"
+
 ###A helper function for getting dataflow strings into the right format
 stringify = function(x){
   z=apply(x, 1, paste, collapse = ", ")
@@ -33,3 +35,41 @@ to_dataflow_string = function(dataflow_sf, file_name){
   stream_out(x, file(paste(c(file_name, ".json"), collapse=  "")))
 }
 
+###process 477 data
+process_477 <-function(D){
+  D$med_dl<-as.numeric(D$med_dl)
+  D$med_ul<-as.numeric(D$med_ul)
+  D$num_con_prov<-as.numeric(D$num_con_prov)
+  names(D)[1]<-"GEOID"
+  D$GEOID<-as.character(D$GEOID)
+  D$GEOID<-as.factor(D$GEOID)
+  return(D)
+}
+
+###loading different time chunks
+load_time_chunks<-function(query){
+  
+  start_loc<-str_locate(query,"GROUP")
+  dec_14<-"WHERE day BETWEEN '2014-07-01' AND '2014-12-01'"
+  jun_15<-"WHERE day BETWEEN '2014-12-01' AND '2015-07-01'"
+  dec_15<-"WHERE day BETWEEN '2015-07-01' AND '2015-12-01'"
+  jun_16<-"WHERE day BETWEEN '2015-12-01' AND '2016-07-01'"
+  dec_16<-"WHERE day BETWEEN '2016-07-01' AND '2016-12-01'"
+  jun_17<-"WHERE day BETWEEN '2016-12-01' AND '2017-07-01'"
+  
+  query_list <- list(dec_14=dec_14, jun_15=jun_15,
+                     dec_15=dec_15, jun_16=jun_16, dec_16=dec_16,jun_17=jun_17)
+  
+  full_D<-data.frame()
+  
+  for(i in 1:length(query_list)){
+    query_list[[i]]<-  str_c(str_sub(query, 1, start_loc[1]-2), 
+                             query_list[[i]], 
+                             str_sub(query, start_loc[1],nchar(query)), collapse="")
+    
+    curr_D<-query_exec(query_list[[i]], project = project, use_legacy_sql=FALSE, max_pages = Inf)
+    full_D<-bind_rows(full_D,data.frame(curr_D, date_range =names(query_list)[i]))
+  }
+  
+  return(full_D)
+}
